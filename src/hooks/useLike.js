bad371bd49b41f2fd9e6687c8f3515efa2a21d15
Marco Mechini <mechinimarco35@gmail.com>
2025-05-02 12:17:36 +0200

    Aggiunta gestione del localStorage per i like nel hook useLike.

--- src/hooks/useLike.js
+++ src/hooks/useLike.js
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useLike() {
 
-    const [like, setLike] = useState([]);
+    const [like, setLike] = useState(JSON.parse(localStorage.getItem('like')) || []);
+
+    useEffect(() => {
+        console.log('lovcal', localStorage);
+        console.log(JSON.parse(localStorage.getItem('like')));
+
+    }, [like])
 
     const handleLike = (id) => {
         id = parseInt(id);
         if (like.includes(id)) {
             setLike((prev) => prev.filter((item) => item !== id));
+            localStorage.setItem('like', JSON.stringify(like.filter((item) => item !== id)))
         } else {
             setLike((prev) => [...prev, id]);
+            localStorage.setItem('like', JSON.stringify([...like, id]))
         }
     }
 
