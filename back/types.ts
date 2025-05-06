9e3689e16c5fb41fd4a10d4d7e5a10f9c1ff427c
Marco Mechini <mechinimarco35@gmail.com>
2025-05-06 11:02:24 +0200

    Aggiunto il file JSON e types.js

---
+++ back/types.ts
@@ -0,0 +1,19 @@
+
+// - Le proprietà `id`, `createdAt` e `updatedAt` vengono aggiunte in automatico dal server
+// - Il nome del tipo (es. `Product`) determinerà il nome dell'endpoint API (es. `/products`)
+// Il server sarà disponibile all'indirizzo: **http://localhost:3001**
+
+
+type NutritionalValue = {
+    name: string; // "Carboidrati"
+    quantity: number; // Quantità del nutriente
+    unit: string;   // g
+  };
+  
+  export type Fruit = {
+
+    title: string;
+    calories: number; // Calorie per 100g
+    category: string; // "Agrume"
+    nutritionalValues: NutritionalValue[]; // Un array di valori nutrizionali
+  };
